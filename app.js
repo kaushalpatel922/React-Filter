8043249495b7df30bc11fb6651f26e6d81658b9d
Kaushal Patel <kaushalpatel922@gmail.com>
2016-09-12 23:06:56 -0400

    data setup

---
+++ app.js
@@ -0,0 +1,8 @@
+var allCardsData = [
+  {name: "AMEX", score: 90, travel: 3, food: 2, other 1},
+  {name: "Visa", score: 100, travel: 2, food: 1, other 1},
+  {name: "CapitalOne", score: 80, travel: 4, food: 2, other 1},
+  {name: "MasterCard", score: 78, travel: 2, food: 2, other 1},
+  {name: "Chase", score: 95, travel: 1, food: 3, other 1},
+  {name: "Citi", score: 88, travel: 2, food: 3, other 1},
+];
