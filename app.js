fa062e337df571037e641f5a8e68555be0273a5f
Kaushal Patel <kaushalpatel922@gmail.com>
2016-09-15 21:58:49 -0400

    added cardslist component

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Defining all the data
 var allCardsData = [
   {name: "AMEX", score: 90, travel: 3, food: 2, other 1},
   {name: "Visa", score: 100, travel: 2, food: 1, other 1},
@@ -6,3 +7,32 @@ var allCardsData = [
   {name: "Chase", score: 95, travel: 1, food: 3, other 1},
   {name: "Citi", score: 88, travel: 2, food: 3, other 1},
 ];
+
+var Control = React.createClass({
+  render: function() {
+    return <p> Sort by: <a>Name</a>, <a>Score</a>, <a>Travel</a>, <a>Food>, <a>Other</a> </p>
+  }
+})
+
+var CardsList = React.createClass({
+  getDefaultProps: function() {
+    return {
+      displayCards: []
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      cards: ''
+    }
+  }
+
+})
+
+//Rendering - This will start rendering to the webpage.
+ReactDOM.render(
+  <div>
+    <h1>Hello React</h1>
+    <CardsList>PlaceHolder Here - pass the props</CardsList>
+  </div>
+)
